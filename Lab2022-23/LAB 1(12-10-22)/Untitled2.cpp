d02a6c851762bb8250d319023d120ea1d7b877f3
Nikolaos Amprazis <94082024+it175107@users.noreply.github.com>
2025-05-27 15:56:11 +0200

    Add files via upload

---
+++ Lab2022-23/LAB 1(12-10-22)/Untitled2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+	printf("DIAMERISMA KOINOXRHSTA\n");	
+
+   printf("IS1             5\n"
+	      "D1              10\n"
+	      "D2              15\n"
+          "D3              20\n");
+	
+	system("Pause");
+}
